42c80c4c4213a5ad18af9be760699e252675ede9
Lucas <35976410+lucasnasc2@users.noreply.github.com>
2024-03-11 14:44:24 +0100

    Update products.js

--- src/store/products.js
+++ src/store/products.js
@@ -71,27 +71,6 @@ export const useProductStore = defineStore("product", {
         console.log(error);
       }
     },
-    async toggleActive(product) {
-      const userStore = useUserStore();
-      const alertStore = useAlertStore();
-      try {
-        let item = JSON.parse(JSON.stringify(product));
-        delete item.id;
-        let productObject = {
-          ...item,
-          createdTimestamp: product.createdTimestamp,
-          active: !item.active,
-          modifiedBy: userStore.user.email,
-          modifiedTimestamp: serverTimestamp(),
-        }
-        const productRef = doc(db, "products", product.id);
-        await setDoc(productRef, { ...productObject }, { merge: true });
-        console.log("document updated");
-      } catch (error) {
-        alertStore.setAlert(error, "error", 5);
-        console.log(error);
-      }
-    },
     async deleteFirestore(product) {
       const alertStore = useAlertStore();
       try {
